7fd0d8a2de6d905d8158a0f81f2bcda4f19ae0ad
carminelentisco <carminelentisco@gmail.com>
2020-06-04 15:42:34 +0200

    Aggiunto secondo oggetto

--- index.php
+++ index.php
@@ -13,15 +13,16 @@ class Movie {
         $this->genere = $_genere;
     }
 
-    public function print($query) {
-        echo "{$this->$query}<br>";
+    public function print() {
+        echo "{$this->title}<br>{$this->author}<br>{$this->publication_date}<br>{$this->genere}<br>";
     }
 }
 
-$movie = new Movie('Volevo nascondermi','Giorgio Diritti','27 febbraio 2020','Drammatico');
+$movie1 = new Movie('Volevo nascondermi','Giorgio Diritti','27 febbraio 2020','Drammatico');
+$movie1->print();
 
-$movie->print('title');
-$movie->print('author');
-$movie->print('publication_date');
-$movie->print('genere');
+echo '<hr>';
+
+$movie2 = new Movie('Fast and Furious','Rob Cohen','21 settembre 2001','Azione/Avventura ');
+$movie2->print();
 
