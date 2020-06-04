e0cf6731a3f4d44e26fba3341db081b32ebf59a8
carminelentisco <carminelentisco@gmail.com>
2020-06-04 16:24:25 +0200

    foglio film

---
+++ partials/movies.php
@@ -0,0 +1,4 @@
+<?php 
+    $movie1 = new Movie('Volevo nascondermi','Giorgio Diritti','27 febbraio 2020','Drammatico');
+    $movie2 = new Movie('Fast and Furious','Rob Cohen','21 settembre 2001','Azione/Avventura ');
+?>
